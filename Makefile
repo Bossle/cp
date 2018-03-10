149ff53c65574cc48626e857dcea83231f964170
Bossle <daniel.bossle@gmail.com>
2018-03-10 12:11:06 -0300

    Add debug build and gcov

--- Makefile
+++ Makefile
@@ -9,8 +9,12 @@ clean :
 	-$(RM) -r bin build lib
 	$(MAKE) -C test clean
 
-install : lib/libossle.a
+install : lib/libossle.a lib/libossle_debug.a
+
 lib/libossle.a : $(patsubst %.cpp,build/%.o,$(shell find src -type f)) | $(parentDirs)
+	ar cr $@ $?
+
+lib/libossle_debug.a : $(patsubst %.cpp,build/%_debug.o,$(shell find src -type f)) | $(parentDirs)
 	ar cr $@ $?
 
 test : install
--- script/cpp.mk
+++ script/cpp.mk
@@ -8,12 +8,13 @@ CXX = g++
 BASEFLAGS = -g -O2 -std=c++11 -static
 INCLUDEFLAGS = $(addprefix -I,$(shell find $(INCLUDEDIRS) -type d))
 WARNINGFLAGS = -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op #-Wshift-overflow=2 -Wduplicated-cond
-DEBUGFLAGS = -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=undefined -fno-sanitize-recover
+DEBUGFLAGS = -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=undefined -fno-sanitize-recover --coverage
 CXXFLAGS = $(BASEFLAGS) $(INCLUDEFLAGS) $(WARNINGFLAGS) $(EXTRAFLAGS)
 
 LIBPATHS = $(shell find $(LIBDIRS) -name *.a)
 LDFLAGS = $(addprefix -L,$(dir $(LIBPATHS)))
-LDLIBS = $(patsubst lib%.a,-l%,$(notdir $(LIBPATHS)))
+LDDEBUGFLAGS = --coverage
+LDLIBS = $(patsubst lib%.a,-l%,$(notdir $(LIBPATHS))) -lubsan
 
 parentDirs = $$(dir $$@)
 
@@ -21,13 +22,19 @@ parentDirs = $$(dir $$@)
 	mkdir -p $@
 
 build/%.d : %.cpp | $(parentDirs)
-	$(CXX) -MM $(CXXFLAGS) $< -MT $@ -MT $(addsuffix .o,$(basename $@)) -MF $@
+	$(CXX) -MM $(CXXFLAGS) $< -MT $@ -MT build/$*.o -MT build/$*_debug.o -MF $@
 
 build/%.o : %.cpp build/%.d | $(parentDirs)
 	$(CXX) -c $(CXXFLAGS) $< -o $@
 
+build/%_debug.o : %.cpp build/%.d | $(parentDirs)
+	$(CXX) -c $(CXXFLAGS) $(DEBUGFLAGS) $< -o $@
+
 bin/% : build/%.o $(LIBPATHS) | $(parentDirs)
 	$(CXX) $(LDFLAGS) $< $(LDLIBS) -o $@
+
+bin/%_debug : build/%_debug.o $(LIBPATHS) | $(parentDirs)
+	$(CXX) $(LDFLAGS) $(LDDEBUGFLAGS) $< $(LDLIBS) -o $@
 
 -include $(shell find build -name *.d)
 
