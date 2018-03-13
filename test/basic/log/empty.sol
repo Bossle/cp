6b9c02136008f64ac0fb8c8de79f625c1cd05902
Bossle <daniel.bossle@gmail.com>
2018-03-13 07:52:20 -0300

    Fix .gitignore
    It was hiding test/log, while the goal was only to hide log/

---
+++ test/basic/log/empty.sol
@@ -0,0 +1,20 @@
+test/basic/log/solver.cpp:4: double 1.3 == 1.3
+test/basic/log/solver.cpp:5: char [6] "hello" == hello
+test/basic/log/solver.cpp:6: std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > std::string("world") == world
+test/basic/log/solver.cpp:7: std::__debug::vector<int, std::allocator<int> > std::vector<int>{1, 2, 3, 4} == {1, 2, 3, 4}
+test/basic/log/solver.cpp:8: std::__debug::deque<int, std::allocator<int> > std::deque<int>{1, 2, 3, 4} == {1, 2, 3, 4}
+test/basic/log/solver.cpp:9: std::__debug::forward_list<int, std::allocator<int> > std::forward_list<int>{1, 2, 3, 4} == {1, 2, 3, 4}
+test/basic/log/solver.cpp:10: std::__debug::list<int, std::allocator<int> > std::list<int>{1, 2, 3, 4} == {1, 2, 3, 4}
+test/basic/log/solver.cpp:11: std::__debug::set<int, std::less<int>, std::allocator<int> > std::set<int>{1, 2, 3, 4} == {1, 2, 3, 4}
+test/basic/log/solver.cpp:12: std::__debug::multiset<int, std::less<int>, std::allocator<int> > std::multiset<int>{1, 1, 2, 3} == {1, 1, 2, 3}
+test/basic/log/solver.cpp:13: std::__debug::map<int, double, std::less<int>, std::allocator<std::pair<int const, double> > > std::map<int, double>{{1, 1.1}, {2, 2.1}, {3, 3.1}, {4, 4}} == {{1, 1.1}, {2, 2.1}, {3, 3.1}, {4, 4}}
+test/basic/log/solver.cpp:14: std::__debug::multimap<int, double, std::less<int>, std::allocator<std::pair<int const, double> > > std::multimap<int, double>{{1, 1.1}, {1, 2.1}, {2, 2.1}, {3, 3}} == {{1, 1.1}, {1, 2.1}, {2, 2.1}, {3, 3}}
+test/basic/log/solver.cpp:15: std::__debug::unordered_set<int, std::hash<int>, std::equal_to<int>, std::allocator<int> > std::unordered_set<int>{1, 2, 3, 4} == {4, 3, 2, 1}
+test/basic/log/solver.cpp:16: std::__debug::unordered_multiset<int, std::hash<int>, std::equal_to<int>, std::allocator<int> > std::unordered_multiset<int>{1, 1, 2, 3} == {3, 2, 1, 1}
+test/basic/log/solver.cpp:17: std::__debug::unordered_map<int, double, std::hash<int>, std::equal_to<int>, std::allocator<std::pair<int const, double> > > std::unordered_map<int, double>{{1, 1.1}, {2, 2.1}, {3, 3.1}, {4, 4}} == {{4, 4}, {3, 3.1}, {2, 2.1}, {1, 1.1}}
+test/basic/log/solver.cpp:18: std::__debug::unordered_multimap<int, double, std::hash<int>, std::equal_to<int>, std::allocator<std::pair<int const, double> > > std::unordered_multimap<int, double>{{1, 1.1}, {1, 2.1}, {2, 2.1}, {3, 3}} == {{3, 3}, {2, 2.1}, {1, 2.1}, {1, 1.1}}
+test/basic/log/solver.cpp:19: std::initializer_list<int> std::initializer_list<int>{1, 2, 3, 4} == {1, 2, 3, 4}
+test/basic/log/solver.cpp:20: std::__debug::array<int, 5ul> std::array<int, 5>{1, 2, 3, 4} == {1, 2, 3, 4, 0}
+test/basic/log/solver.cpp:21: std::__debug::bitset<100ul> std::bitset<100>("0100110") == 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100110
+test/basic/log/solver.cpp:22: std::pair<int, double> std::pair<int, double>{3, 4.0} == {3, 4}
+test/basic/log/solver.cpp:23: std::tuple<int, double, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > std::tuple<int, double, std::string>{2, 3.14, "test"} == {2, 3.14, test}
